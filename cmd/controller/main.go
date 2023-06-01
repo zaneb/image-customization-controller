30fa7fdea8a3c546b1f4f2e8594f6fe5beeb06af
Zane Bitter <zbitter@redhat.com>
2023-06-02 10:22:10 +1200

    OCPBUGS-13532: Actually respond to changes in Secrets
    Somehow I managed to miss out the most important part of the change in
    e920fe9a8340eb5f28273088020bc5686cfafcb6.

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -23,7 +23,6 @@ import (
 	"os"
 	"time"
 
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	k8sruntime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/selection"
@@ -31,7 +30,6 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -90,11 +88,10 @@ func runController(watchNamespace string, imageServer imagehandler.ImageHandler,
 	}
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                scheme,
-		Port:                  0, // Add flag with default of 9443 when adding webhooks
-		Namespace:             watchNamespace,
-		ClientDisableCacheFor: []client.Object{&corev1.Secret{}},
-		NewCache:              cache.BuilderWithOptions(cacheOptions),
+		Scheme:    scheme,
+		Port:      0, // Add flag with default of 9443 when adding webhooks
+		Namespace: watchNamespace,
+		NewCache:  cache.BuilderWithOptions(cacheOptions),
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
